c9b62090786872fff8929cba6e420f09f95d84b6
aditya478492 <jonnaadi@gmail.com>
2024-04-12 01:31:13 -0400

    added login page images

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("com.android.application")
 }
 
+
 android {
     namespace = "edu.northeastern.demostructure"
     compileSdk = 34
@@ -39,4 +40,5 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.17")
 }
