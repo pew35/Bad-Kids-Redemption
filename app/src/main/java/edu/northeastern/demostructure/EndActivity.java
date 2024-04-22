0c24ed0e56f7a740ab0dfc528be0c1521397ebc5
Peiyi Wu <pwggywu226@gmail.com>
2024-04-21 20:24:08 -0700

    ending story

--- app/src/main/java/edu/northeastern/demostructure/EndActivity.java
+++ app/src/main/java/edu/northeastern/demostructure/EndActivity.java
@@ -15,6 +15,13 @@ public class EndActivity extends AppCompatActivity {
     Images images;
     ImageView endchar;
     TextView endStory;
+
+    String cook = "Guided by a passion for culinary creativity, you rise to fame as a master chef, delighting palates with your delectable dishes and innovative flavors. Your culinary creations bring joy and warmth to all who taste them, a testament to the transformative power of food and love.";
+    String magic = "Embracing the arcane arts, you master the ancient secrets of magic, wielding its power with grace and precision. With each incantation spoken and spell cast, you reshape reality itself, a sorcerer of unparalleled skill and wisdom.";
+    String charm = "Armed with charisma and charm, you navigate the complexities of human connection with ease, winning over hearts and minds wherever you go. Your magnetic personality leaves a lasting impression on all who cross your path, transforming even the darkest of days into moments of joy.";
+    String knowledge = "Driven by a thirst for knowledge, you delve deep into the realm of science, unlocking the mysteries of the universe with each discovery. Your brilliance shines bright, illuminating the path for future generations to follow in your footsteps.";
+    String fighter = "Fueled by the fire of resilience, you emerge as a formidable fighter, channeling the pain of your past into strength and determination. With every battle won, you carve out a place for yourself in the world, a beacon of hope for others facing similar struggles.";
+    String lowSan = "Lost in the depths of despair, you succumb to madness, the echoes of bullying and isolation haunting your every thought. Alone and forgotten, you wander the halls of your school, a ghost of your former self, lost to the darkness within.";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,13 +34,21 @@ public class EndActivity extends AppCompatActivity {
         boy = getIntent().getBooleanExtra("boy", true);
         ending = getIntent().getIntExtra("image",-1);
 
-        if(boy == true){
+        if(boy){
             endchar.setImageResource(R.drawable.male);
         } else {
             endchar.setImageResource(R.drawable.female);
         }
-        String path = getIntent().getStringExtra("path");
-        endStory.setText(path);
+        String endString = "";
+        switch (ending){
+            case -1 : endString = lowSan; break;
+            case 1 : endString = fighter; break;
+            case 2 : endString = knowledge; break;
+            case 3 : endString = charm; break;
+            case 4 : endString = magic; break;
+            case 5 : endString = cook; break;
+        }
+        endStory.setText(endString);
 
         images = new Images(boy);
 
